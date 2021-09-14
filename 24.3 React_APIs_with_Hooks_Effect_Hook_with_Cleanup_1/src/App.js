45d01e14c64d65c1ceb9b34862ea35e5eb49bb0b
Tyler Williams <tylerwilliams1737@gmail.com>
2021-09-14 08:27:05 -0700

    Added: 24.3

---
+++ 24.3 React_APIs_with_Hooks_Effect_Hook_with_Cleanup_1/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+
+  // Load data from https://jsonplaceholder.typicode.com/todos?userId=3
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <ul className="todo-list">
+        {toDos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "",
+            }}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
