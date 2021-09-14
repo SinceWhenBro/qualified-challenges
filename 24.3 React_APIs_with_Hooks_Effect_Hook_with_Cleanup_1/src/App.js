3024b663a937ede708e577c0940240de5c431dd6
Tyler Williams <tylerwilliams1737@gmail.com>
2021-09-14 08:41:57 -0700

    Finished: 24.3

--- 24.3 React_APIs_with_Hooks_Effect_Hook_with_Cleanup_1/src/App.js
+++ 24.3 React_APIs_with_Hooks_Effect_Hook_with_Cleanup_1/src/App.js
@@ -4,6 +4,30 @@ function App() {
   const [toDos, setToDos] = useState([]);
 
   // Load data from https://jsonplaceholder.typicode.com/todos?userId=3
+  useEffect(
+    () => {
+      
+      const abortController = new AbortController();
+      //do work
+      async function loadData(){
+        try {
+          const res = await fetch (
+            "https://jsonplaceholder.typicode.com/todos?userId=3",
+            { signal: abortController.signal }
+            );
+          const data = await res.json();
+          setToDos(data);
+        } catch (error) {
+          if(error.name === "AbortError") {
+            console.log("Aborted", userID);
+          } else {
+            throw error;
+          }
+        }
+      }
+      loadData();
+      return () => abortController.abort();
+    },[]);
 
   return (
     <div className="App">
