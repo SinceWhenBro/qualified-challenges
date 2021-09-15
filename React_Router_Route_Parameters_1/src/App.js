5b3a8dee4e736ab2e78db7213d9c9284c1c6bbfd
Tyler Williams <tylerwilliams1737@gmail.com>
2021-09-15 09:16:18 -0700

    Added: 25.4

---
+++ React_Router_Route_Parameters_1/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    // No need to add <Router>, it has been added to ./index.js
+    <div className="App">
+      <Link to="/user/new">New User</Link>
+      {Array(10)
+        .fill()
+        .map((ignoredValue, index) => index + 1)
+        .map((id) => (
+          <div key={id}>
+            <Link to={`/user/${id}`} data-testid={`user-${id}`}>
+              User{id}
+            </Link>
+          </div>
+        ))}
+      // Setup routes with route paramaters as needed
+    </div>
+  );
+}
+
+export default App;
