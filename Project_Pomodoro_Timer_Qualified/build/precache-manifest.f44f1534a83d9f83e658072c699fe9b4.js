1c862758cee27deada511bf9277154fcab671c46
Tyler Williams <tylerwilliams1737@gmail.com>
2021-09-20 07:53:15 -0700

    Added: FlashCard Capstone

---
+++ Project_Pomodoro_Timer_Qualified/build/precache-manifest.f44f1534a83d9f83e658072c699fe9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7681c96b70eb76dedd490dba74544cc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce3d9051949c229849c",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce3d9051949c229849c",
+    "url": "/static/js/main.fe7f5c4b.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
