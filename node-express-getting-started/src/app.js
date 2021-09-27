fba9e8b5be38dff6b3d4f09598df7ea30bed7dbc
Tyler Williams <tylerwilliams1737@gmail.com>
2021-09-27 09:17:03 -0700

    Added: Routes to Server

--- node-express-getting-started/src/app.js
+++ node-express-getting-started/src/app.js
@@ -1,4 +1,19 @@
+//import
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+//middleware functions
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+  };
+
+
+
+//instantiating middleware
+//using morgan to console log live
+app.use(morgan("dev"));
+app.get("/hello", sayHello);
+
+//exporting the express function
 module.exports = app;
--- node-express-getting-started/src/server.js
+++ node-express-getting-started/src/server.js
@@ -1,5 +1,5 @@
 const { PORT = 5000 } = process.env;
 const app = require("./app");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const listener = () => console.log(`Listening on Port Yw ${PORT}!`);
 app.listen(PORT, listener);
